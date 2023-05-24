5da88be1a4a774658c79b12935f738fe1ec38afd
CvRain <cvraindays@outlook.com>
2023-05-24 14:57:01 +0800

    添加了系统启的配置文件

--- src/app.cpp
+++ src/app.cpp
@@ -4,10 +4,56 @@
 #include <oatpp/network/Server.hpp>
 #include <oatpp/core/macro/codegen.hpp>
 
-void run() {
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <toml++/toml.h>
+#include <optional>
+
+void run(const std::string &host = "localhost", uint16_t port = 5281);
+void create_setting();
+std::optional<std::string> reading_setting(const std::filesystem::path& path);
+
+int main() {
+    oatpp::base::Environment::init();
+
+    //初始化服务器信息，读取settings.toml
+    std::filesystem::path setting_path{"./settings.toml"};
+    //检查文件是否存在，不存在就创建
+    if(!std::filesystem::exists(setting_path)){
+        //create default file
+        create_setting();
+    }
+    //读取文件并且尝试反序列化，失败则使用默认配置
+    std::string host;
+    int port;
+    if(auto result = reading_setting(setting_path); !result.has_value()){
+        OATPP_LOGI("Setting","Loading::Invalid");
+        return -1;
+    }else{
+        auto table = toml::parse(result.value());
+        OATPP_LOGI("Setting","Loading file");
+        const auto result_host = table["host"].value<std::string>();
+        const auto result_port = table["port"].value<int>();
+        if(result_host.has_value() && result.has_value()){
+            host = result_host.value();
+            port = result_port.value();
+        }
+        OATPP_LOGI("Setting","Host: %s", host.data());
+        OATPP_LOGI("Setting","Port: %d", port);
+    }
+
+    run(host,port);
+
+    /* Destroy oatpp Environment */
+    oatpp::base::Environment::destroy();
+}
+
+
+void run(const std::string &host, const uint16_t port) {
 
     //注册一个应用服务组件
-    AppComponent::Component components;
+    AppComponent::Component components(host, port);
 
     //获得路由组建
     OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
@@ -25,17 +71,31 @@ void run() {
     oatpp::network::Server server(connectionProvider, connectionHandler);
 
     //打印服务器初始化信息
-    OATPP_LOGI("CvlJoint", "Listening on port %s", connectionProvider->getProperty("port").getData());
+    OATPP_LOGI("CvlJoint", "Listening: %s", connectionProvider->getProperty("port").getData());
 
     /* Run server */
     server.run();
 }
 
-int main() {
-    oatpp::base::Environment::init();
+void create_setting()
+{
+    auto default_table = toml::table{
+            {"host","localhost"},
+            {"port",9280}
+    };
+    OATPP_LOGI("Setting","Create::Default");
+    std::stringstream ss{};
+    ss << default_table;
 
-    /* Run App */
-    run();
+    std::ofstream file("./settings.toml");
+    file << ss.str();
+    file.close();
+}
 
-    /* Destroy oatpp Environment */
-    oatpp::base::Environment::destroy();}
+std::optional<std::string> reading_setting(const std::filesystem::path &path) {
+    std::ifstream setting_file(path);
+    std::stringstream ss;
+    ss << setting_file.rdbuf();
+    setting_file.close();
+    return ss.str();
+}
--- src/component.hpp
+++ src/component.hpp
@@ -6,43 +6,51 @@
 #define CVL_JOINT_COMPONENT_HPP
 
 #include <iostream>
+#include <string>
 #include <oatpp/parser/json/mapping/ObjectMapper.hpp>
 #include <oatpp/web/server/HttpConnectionHandler.hpp>
 #include <oatpp/network/tcp/server/ConnectionProvider.hpp>
 #include <oatpp/core/macro/component.hpp>
 
-using oatpp::network::ServerConnectionProvider;
-using oatpp::network::tcp::server::ConnectionProvider;
-using oatpp::web::server::HttpRouter;
-using oatpp::network::ConnectionHandler;
-using oatpp::web::server::HttpConnectionHandler;
-using oatpp::data::mapping::ObjectMapper;
-
 namespace AppComponent {
+    using oatpp::network::ServerConnectionProvider;
+    using oatpp::network::tcp::server::ConnectionProvider;
+    using oatpp::web::server::HttpRouter;
+    using oatpp::network::ConnectionHandler;
+    using oatpp::web::server::HttpConnectionHandler;
+    using oatpp::data::mapping::ObjectMapper;
+    using oatpp::base::Environment;
 
     class Component {
     public:
-        //创建服务连接组件，用来监听端口
-        OATPP_CREATE_COMPONENT(std::shared_ptr<ServerConnectionProvider>, serverConnectionProvider)([](){
-            return ConnectionProvider::createShared({"localhost",5281, oatpp::network::Address::IP_4});
-        }());
+        explicit Component(const std::string &server_host, const uint16_t server_port)
+                : serverConnectionProvider(
+                Environment::Component<std::shared_ptr<ServerConnectionProvider>>{   //创建服务连接组件，用来监听端口
+                        ConnectionProvider::createShared({server_host, server_port, oatpp::network::Address::IP_4})
+                }) {
+            OATPP_LOGI("CvlJoint", "Component::Initialize");
+        }
 
         //创建路由组建
-        OATPP_CREATE_COMPONENT(std::shared_ptr<HttpRouter>, httpRouter)([](){
-           return HttpRouter::createShared();
+        OATPP_CREATE_COMPONENT(std::shared_ptr<HttpRouter>, httpRouter)([]() {
+            return HttpRouter::createShared();
         }());
 
         //创建用于路由组建连接请求的连接管理器组建
-        OATPP_CREATE_COMPONENT(std::shared_ptr<ConnectionHandler>, serverConnectionHandler)([](){
+        OATPP_CREATE_COMPONENT(std::shared_ptr<ConnectionHandler>, serverConnectionHandler)([]() {
             OATPP_COMPONENT(std::shared_ptr<HttpRouter>, router);
             return HttpConnectionHandler::createShared(router);
         }());
 
         //创建Dto对象管理组件用于序列化和返序列化类
-        OATPP_CREATE_COMPONENT(std::shared_ptr<ObjectMapper>, apiObjectMapper)([](){
+        OATPP_CREATE_COMPONENT(std::shared_ptr<ObjectMapper>, apiObjectMapper)([]() {
             return oatpp::parser::json::mapping::ObjectMapper::createShared();
         }());
+
+    public:
+        Environment::Component<std::shared_ptr<ServerConnectionProvider>> serverConnectionProvider;
     };
+
 }
 
 #endif //CVL_JOINT_COMPONENT_HPP
